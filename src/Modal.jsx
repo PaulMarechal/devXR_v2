b58dc2fe5f73d9520108dfd667978443f372c12e
Paul Marechal <paulmarechal785@hotmail.fr>
2024-07-24 15:07:21 +0200

    Update css + added img and video on catacombes presentation page

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -42,19 +42,19 @@ export default function Modal() {
                     <div className="text_catacombes_page" >
                         <h1 id="title_catacombes_page" >Découvrez les Catacombes Interdites<br/> de Paris avec catacombes.xyz</h1>
                         <p id="first_text_catacombes_page" >
-                            Plongez dans l'histoire secrète de Paris
-                            <span>grâce à notre projet innovant, catacombes.xyz. Ce site unique vous offre une exploration immersive des catacombes interdites de la Rive Gauche de Paris. Découvrez les carrières de Paris comme jamais auparavant, avec des visites en 3D, des expériences de réalité virtuelle (VR) et de réalité augmentée (AR).</span>
+                            Plongez dans l'histoire secrète de Paris 
+                            <span> grâce à notre projet innovant, <b>catacombes.xyz</b>. Ce site unique vous offre une <b>exploration immersive</b> des catacombes interdites de la Rive Gauche de Paris. Découvrez les carrières de Paris comme jamais auparavant, avec des <b>visites en 3D</b>, des expériences de <b>réalité virtuelle (VR) et de réalité augmentée (AR)</b>.</span>
                         </p>
                         <div id="second_div_text_catacombes_page">
                             <h4>Photogrammétrie de Haute Précision</h4>
-                            <p>Grâce à la technologie de photogrammétrie avancée, chaque salle des catacombes a été minutieusement scannée et modélisée en 3D. Cette technique permet de capturer les moindres détails, offrant une représentation fidèle et époustouflante de ces espaces souterrains historiques.</p>
+                            <p>Grâce à la technologie de <b>photogrammétrie avancée</b>, chaque salle des catacombes a été minutieusement scannée et modélisée en 3D. Cette technique permet de capturer les moindres détails, offrant une <b>représentation fidèle</b> et époustouflante de ces espaces souterrains historiques.</p>
                         </div>
 
                         <img  id="first_image_room_guerinet" src="https://devxr.fr/assets/images/catacombes/catacombes_page/guerinet_room_catacombes.png" alt="Salle Guerinet - catacombes interdites de Paris" />
 
-                        <h4 className="title_modale_presentation">Visites en 3D et VR</h4>
+                        <h4 className="title_modale_presentation">Visites Virtuelles</h4>
                         <div className="div_video_catacombes">
-                            <img src="https://devxr.fr/assets/images/catacombes/oculus_screen_guerinet_catacombes.jpg" alt="" />
+                            <img src="https://devxr.fr/assets/images/catacombes/oculus_screen_guerinet_catacombes.jpg" alt="Capture d'écran d'une visite des catacombes interdites avec un casque Oculus Quest 2 en réalité virtuelle." />
 
                             <video src="https://devxr.fr/assets/video/visite_guerinet_realite_virtuelle.mp4" loop autoPlay={true} muted></video>
                             
@@ -62,32 +62,46 @@ export default function Modal() {
                         </div>
 
                         <div className="div_text_under_images">
-                            <p>Naviguez à travers les catacombes directement depuis votre navigateur web, avec une expérience totalement en 3D. Pour une immersion encore plus profonde, utilisez un casque de réalité virtuelle.</p>
-                            <p>Explorez chaque recoin comme si vous y étiez, ressentez l'ambiance unique et découvrez les secrets enfouis sous la ville lumière.</p>
+                            <p>Naviguez à travers les catacombes directement depuis votre <b>navigateur web</b>, avec une expérience totalement en 3D. Pour une immersion encore plus profonde, utilisez un <b>casque de réalité virtuelle</b>.</p>
+                            <p>Explorez chaque recoin comme si vous y étiez, ressentez l'<b>ambiance unique</b> et découvrez les secrets enfouis sous la ville lumière.</p>
                         </div>
 
-                        <h4 >Réalité Augmentée sur Mobile</h4>
 
                         <div className="second_div_video_text">
                             <video src="https://devxr.fr/assets/video/demo_catacombes_realite_virtuelle.mp4" loop muted autoPlay={true} ></video>
-                            <p>Transformez votre smartphone en une fenêtre vers les catacombes. Scannez des cartes spécifiques que nous avons dispersées dans les carrières et regardez les salles en 3D apparaître directement sur votre écran. Promenez-vous librement à travers ces représentations virtuelles, examinez la taille, la hauteur et les détails fascinants de chaque salle.</p>
+                            <span>
+                                <h4 >Réalité Augmentée sur Mobile</h4>
+                                <p>Transformez votre smartphone en une fenêtre vers les catacombes. <b>Scannez des cartes</b> spécifiques que nous avons dispersées dans les carrières et regardez les salles en 3D <b>apparaître directement</b> en réalité augmentée. Promenez-vous librement à travers ces représentations virtuelles, examinez la <b>taille</b>, la <b>hauteur</b> et les <b>détails fascinants</b> de chaque salle.</p>
+                            </span>
                         </div>
 
-                        <h4 >Cartes et Interactions</h4>
                         <div className="third_div_video_text">
-                            <p >Sur notre site, nous avons également inclus une section dédiée aux cartes des carrières. Chaque carte, lorsqu'elle est scannée avec votre téléphone, fait apparaître une salle en réalité augmentée. Cette fonctionnalité interactive permet une nouvelle dimension d'exploration et de découverte, offrant une perspective unique sur la taille et la composition des catacombes.</p>
-                            <video src="https://devxr.fr/assets/video/demo_telephone_realite_augmentee.mp4"></video>
                             <span>
-                                <img src="http://devxr.fr/assets/images/catacombes/qr_cabinet_old.png" alt="" />
-                                <img src="http://devxr.fr/assets/images/catacombes/qr_guerinet.png" alt="" />
+                                <h4>Cartes et Interactions</h4>
+                                <p >Sur notre site, nous avons également inclus une section dédiée aux cartes des carrières. Chaque carte, lorsqu'elle est <b>scannée avec votre téléphone</b>, fait apparaître une <b>salle en réalité augmentée</b>. Cette fonctionnalité interactive permet une nouvelle dimension d'<b>exploration</b> et de <b>découverte</b>, offrant une <b>perspective unique</b> sur la taille et la composition des catacombes.</p>
+                            </span>
+                            <video src="https://devxr.fr/assets/video/demo_telephone_realite_augmentee.mp4" loop muted autoPlay={true}></video>
+                            <span>
+                                {/* <img src="http://devxr.fr/assets/images/catacombes/qr_cabinet_old.png" alt="" /> */}
+                                <span>
+                                    <p>V1</p>
+                                    <p>V2</p>
+                                </span>
+                                <img src="http://devxr.fr/assets/images/catacombes/qr_code_guerinet.png" alt="QR code qui permet d'afficher la salle Guerinet des Catacombes interdites de Paris en réalité augmentée" />
+                                <img src="http://devxr.fr/assets/images/catacombes/qr_code_fdc.png" alt="QR code qui permet d'affciher la salle Bracitorium des Catacombes interdites de Paris en réalité augmentée" />
                             </span>
                         </div>
 
 
-                        <h4 >Une Exploration Immersive</h4>
-                        <p >catacombes.xyz n'est pas seulement un site web, c'est une véritable aventure numérique. Que vous soyez un passionné d'histoire, un amateur de technologie ou simplement curieux, notre plateforme vous offre une expérience immersive et éducative sans pareille. Plongez dans l'obscurité mystérieuse des catacombes de Paris et découvrez des salles cachées, des histoires oubliées et des merveilles architecturales sous un nouvel angle.</p>
+                        <h4 className="title_immersive_exploration">Une Exploration Immersive</h4>
 
-                        <p >Visitez <a href="https://catacombes.xyz">catacombes.xyz</a> dès aujourd'hui et laissez-vous transporter dans les profondeurs historiques de Paris.</p>
+                        <div className="div_text_under_images">
+                            <p><b>Catacombes.xyz</b> n'est pas seulement un site web, c'est une véritable <b>aventure numérique</b>. Que vous soyez un passionné d'histoire, un amateur de technologie ou simplement curieux, notre plateforme vous offre une <b>expérience immersive</b> et éducative sans pareille. </p>
+                            <p>Plongez dans l'obscurité mystérieuse des catacombes de Paris et découvrez des <b>salles cachées</b>, des <b>histoires oubliées</b> et des <b>merveilles architecturales</b> sous un nouvel angle.</p>
+                        </div>
+
+
+                        <p className="visit_website_text">Visitez <b><a href="https://catacombes.xyz" target="_blank">catacombes.xyz</a></b> dès aujourd'hui et laissez-vous transporter dans les profondeurs historiques de Paris.</p>
                     </div>
                 </div>
 
