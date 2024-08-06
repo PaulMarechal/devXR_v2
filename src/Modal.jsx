ea4c9149602d7d9a2e1ae6ad8835f444773da9d6
Paul Marechal <paulmarechal785@hotmail.fr>
2024-08-06 18:28:26 +0200

    Update 'contact us' page

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -589,18 +589,13 @@ export default function Modal() {
                         </div>
 
                         <div>
-                            <p><b>Envoyez-nous un message</b> et nous tacherons de vous recontacter dans les meilleurs délais. </p>
+                            <p className="align_conter"><b>Envoyez-nous un message</b> et nous tacherons de vous recontacter dans les meilleurs délais. </p>
                                 
                             <div>
                                 <a href="contact@paulmarechal.xyz">
                                     <svg id="contact_mail" xmlns="http://www.w3.org/2000/svg"  width="50"  height="50"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="1"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-mail"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z" /><path d="M3 7l9 6l9 -6" /></svg>
                                     Mail
                                 </a>
-                                <a href="tel:+33648357374">
-                                    <svg id="contact_tel" xmlns="http://www.w3.org/2000/svg"  width="50"  height="50"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="1"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-phone"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" /></svg>
-                                    Téléphone
-                                </a>
-
                             </div>
                              
                         </div>
