11783ae6fabd568eb0f0a2c42a80f4dec3b2d1c2
Paul Marechal <paulmarechal785@hotmail.fr>
2024-08-20 23:12:56 +0200

    Update menu mobile css

--- src/Interface_mobile.jsx
+++ src/Interface_mobile.jsx
@@ -3,21 +3,24 @@ import $ from "jquery";
 
 export default function Interface_mobile() {
 
-  useEffect(() => {
-    $(".close_icon_start").on("click", function () {
-      $(".interface").css("opacity", "0");
-      setTimeout(() => {
-        $(".interface").css("display", "none");
-      }, 400);
-    });
-
-    return () => {
-      $(".close_icon_start").off("click");
-    };
-  }, []);
+    useEffect(() => {
+        const handleClose = () => {
+            $(".interface_mobile").css("opacity", "0");
+            setTimeout(() => {
+                $(".interface_mobile").css("display", "none");
+            }, 400);
+        };
+      
+        $(".close_icon_start").on("touchend", handleClose);
+      
+        return () => {
+            $(".close_icon_start").off("touchend", handleClose);
+        };
+    }, []);
+      
 
   return (
-    <div className="interface">
+    <div className="interface_mobile">
         <div className="welcome_modal">
             <div className="close_icon_start" title="Fermer la fenêtre">
                 <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-x">
@@ -30,6 +33,10 @@ export default function Interface_mobile() {
                 <span>Bienvenue sur </span>
                 <br />
                 <span className="name_font">DevXR.fr</span>
+            </p>
+            <p className="text_intro">
+                Que ce soit pour un site web classique qui valorise votre entreprise ou des expériences 
+                immersives en réalité augmentée, virtuelle ou mixte, nous avons ce qu'il vous faut.
             </p>
             <p className="text_intro">
                 Pour profiter pleinement du site web en version immersive merci de vous 
