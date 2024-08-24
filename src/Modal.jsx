892dff71859769dc66b99d72fba86a896965675a
Paul Marechal <paulmarechal785@hotmail.fr>
2024-08-24 17:53:02 +0200

    Added .htaccess file

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -699,7 +699,7 @@ export default function Modal() {
                             <p className="align_conter"><b>Envoyez-nous un message</b> et nous tacherons de vous recontacter dans les meilleurs délais. </p>
                                 
                             <div>
-                                <a href="contact@paulmarechal.xyz">
+                                <a href="mailto:contact@paulmarechal.xyz">
                                     <svg id="contact_mail" xmlns="http://www.w3.org/2000/svg"  width="50"  height="50"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="1"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-mail"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z" /><path d="M3 7l9 6l9 -6" /></svg>
                                     Mail
                                 </a>
